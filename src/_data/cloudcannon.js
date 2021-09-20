7259647412036d0cb6a7a973dabc0c9ff30220e8
Ben White <hello@bemm.com.au>
2021-09-21 01:56:15 +1000

    add cloudcannon data file

---
+++ src/_data/cloudcannon.js
@@ -0,0 +1,20 @@
+module.exports = {
+  _collection_groups: [
+    {
+      heading: 'Site',
+      collections: ['pages', 'drafts']
+    },
+    {
+      heading: 'News',
+      collections: ['blog']
+    },
+    {
+      heading: 'Other',
+      collections: [
+        'navigation',
+        'footer',
+        'seo'
+      ]
+    }
+  ]
+};
